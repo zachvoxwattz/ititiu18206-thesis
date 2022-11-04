a7b687e3832d700d7be97fb18f87e325df9f0314
Võ C. Kha <revengemultikill@gmail.com>
2022-11-05 02:02:16 +0700

    v1.6b
    - Removed Java Event Replay App due to its liability.
    - Started implementation using Nodefluent's Kafka Streams

--- JavaEventReplayApp/demo/main/ProgramRunner.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class ProgramRunner {
-	public static void main(String[] args) {
-		JKafkaStream rp = new JKafkaStream("JApp", "Charlotte:9091", "tbSortedResults");
-		
-		rp.startStream();
-	}
-}
--- JavaEventReplayApp/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/** @author zachwkero **/
-module JavaEventReplayApp {
-	requires java.desktop;
-	requires org.slf4j;
-	requires com.fasterxml.jackson.databind;
-	requires kafka.streams;
-	requires kafka.clients;
-}
